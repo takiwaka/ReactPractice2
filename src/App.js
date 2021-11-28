6a90ae97fb268e61822200b32849010bd698377e
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-11-29 06:01:49 +0900

    CSS Modules使い方

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import { useState, useCallback } from "react";
-import { Child } from "./components/Child";
+import { CssModules } from "./components/CssModules";
 import "./styles.css";
 
 export default function App() {
-  console.log("reflesh");
-
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-  const onClickOpen = () => setOpen(!open);
-  const onClickClose = useCallback(() => setOpen(false), []);
-
-  return (
-    <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <Child open={open} onClickClose={onClickClose} />
-    </div>
-  );
+  return <CssModules />;
 }
